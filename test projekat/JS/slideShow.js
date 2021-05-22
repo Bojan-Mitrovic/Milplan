ad552b334987aa99d827af607931749c11b16834
Bojan <bojanmitrovic@hotmail.rs>
2021-05-22 22:55:06 +0200

    almost finished

--- test projekat/JS/slideShow.js
+++ test projekat/JS/slideShow.js
@@ -3,22 +3,18 @@ $(document).ready(function() {
     $('.img-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        // infinite: true,
-        // autoplay: true,
-        // autoplaySpeed: 600,
-        // adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
         arrows: false,
         fade: true
     });
     $('.txt-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 600,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
         dots: true,
-        centerMode: true,
         arrows: false,
-        adaptiveHeight: true,
-        focusOnSelect: true
     });
 });
